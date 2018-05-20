4f5b189b0b4cf9233fdb06cd712278134daa250a
Shivam Mamgain <shivammamgain47@gmail.com>
2018-05-20 13:52:04 +0530

    Print stack in bucket
    Add test

--- stack/stack.go
+++ stack/stack.go
@@ -2,29 +2,69 @@ package stack
 
 import (
 	"fmt"
+	"strings"
+)
+
+const (
+	BoxVer     = "│"
+	BoxHor     = "─"
+	BoxUpRight = "└"
+	BoxUpLeft  = "┘"
+	BoxUpHor   = "┴"
 )
 
 // Stack represents a stack of elements.
 type Stack interface {
-	Pop() (interface{}, bool)
+	// Pop method must pop the top element out of the stack and return it. In
+	// case of an empty stack ok should be false, else true.
+	Pop() (ele interface{}, ok bool)
+	// Push method must insert an element in the stack. Since ele is of interface
+	// type, type assertion must be done before inserting element in the stack.
 	Push(ele interface{})
 }
 
+// Print prints the formatted stack to standard output.
 func Print(s Stack) {
-	elems := []interface{}{}
-	for {
-		if ele, ok := s.Pop(); ok {
-			elems = append(elems, ele)
-			continue
-		}
-		break
-	}
+	fmt.Print(Sprint(s))
+}
 
-	for _, ele := range elems {
-		fmt.Println(ele)
+// Sprint returns the formatted stack.
+func Sprint(s Stack) (str string) {
+	elems := []interface{}{}
+	e, ok := s.Pop()
+	for ok {
+		elems = append(elems, e)
+		e, ok = s.Pop()
 	}
 
 	for i := len(elems) - 1; i >= 0; i-- {
 		s.Push(elems[i])
 	}
+
+	// w is value column width
+	vals, w := []string{}, 0
+	for _, e := range elems {
+		v := fmt.Sprintf("%v", e)
+		vals = append(vals, v)
+		if len(v) > w {
+			w = len(v)
+		}
+	}
+	// position column width
+	posW := digitWidth(len(vals))
+
+	for i, v := range vals {
+		str += fmt.Sprintf("%s%+*s%s%*d%s\n", BoxVer, w, v, BoxVer, posW, len(vals)-i, BoxVer)
+	}
+	str += fmt.Sprint(BoxUpRight, strings.Repeat("─", w), BoxUpHor, strings.Repeat("─", posW), BoxUpLeft, "\n")
+
+	return
+}
+
+func digitWidth(d int) (w int) {
+	for d != 0 {
+		d = d / 10
+		w++
+	}
+	return
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -9,24 +9,58 @@ type MyStack struct {
 	elems []int
 }
 
-func (s *MyStack) Pop() (interface{}, bool) {
+func (s *MyStack) pop() (int, bool) {
 	l := len(s.elems)
 	if l == 0 {
-		return "", false
+		return 0, false
 	}
 	ele := s.elems[l-1]
 	s.elems = s.elems[:l-1]
 	return ele, true
 }
 
+func (s *MyStack) push(ele int) {
+	s.elems = append(s.elems, ele)
+}
+
+func (s *MyStack) Pop() (interface{}, bool) {
+	return s.pop()
+}
+
 func (s *MyStack) Push(ele interface{}) {
-	s.elems = append(s.elems, ele.(int))
+	s.push(ele.(int))
 }
 
 func TestPrint(t *testing.T) {
 	s := MyStack{}
-	s.Push(10)
-	s.Push(19)
-	s.Push(11)
-	stack.Print(&s)
+	s.push(10)
+	s.push(199)
+	s.push(1111)
+	s.push(1111)
+	s.push(12111)
+	s.push(-113333111)
+	s.push(1111)
+	s.push(19)
+	s.push(199)
+	s.push(199)
+	s.push(121499)
+	s.push(-19019)
+  want := `│    -19019│12│
+│    121499│11│
+│       199│10│
+│       199│ 9│
+│        19│ 8│
+│      1111│ 7│
+│-113333111│ 6│
+│     12111│ 5│
+│      1111│ 4│
+│      1111│ 3│
+│       199│ 2│
+│        10│ 1│
+└──────────┴──┘
+`
+  got := stack.Sprint(&s)
+  if got != want {
+    t.Errorf("Expected:\n%s\n\nGot:\n%s\n", want, got)
+  }
 }
